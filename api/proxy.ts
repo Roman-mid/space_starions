077e0e26565e1b913d152a7b0ac979d0a975d6b6
Roman-mid <roman.midianko@gmail.com>
2025-05-15 21:02:13 +0100

    check with stric token

--- api/proxy.ts
+++ api/proxy.ts
@@ -10,7 +10,9 @@ export default async function handler(req, res) {
 
   const { station = '', artifact = '', historic = 'false' } = req.query;
 
-  const token = req.headers.jfwt;
+  // const token = req.headers.jfwt;
+  const token =
+    'Yryr7KWg92Ms1B4Goqeuf-5R-dKBK4F3Obbb-fYSGTRTU6gLdLzG9rFjhE1BZNs7nHufxtjFcwXCUhtMbFgca0WReloJ1DZHj_hZigYtcF7x-SuZwUuvHIvqQMDgrG6JQ6NxCILAChdR_G6fy1Z9RzL_JcQoBDchXM9Cs5bAADq2Fjl--O4K0_4w4g-gPAPbwmHRHWDpUWwR7Ah0UdpCFiRkF3ssrhXI7k_C60kVU3IHXLWZ5EXAuS3MzHyE0ULV8tebADxH8qv6ni-rjAsiu2xt9T8w2lHtq2b_Px-U9_eEw4DoQGhMkO5IQAiMlVYnumwawDphfEp5yL5WgC8G88OHCM68AHEatCIZzcxTs6ffRGPa_yAXLMgs2CJYdERX2yX1Ut2CB7Q72yz8kHoMfoilZ90ECFPacUY4-j66I1nHnGBf4y39Yyg2swAG.Yryr7KWg92Ms1B4Goqeufw==';
 
   console.log('Received query:', req.query);
   console.log('Token:', token);
