af8b51a08a7fd96645d6b12d9e8acadbefb3efb7
Roman-mid <roman.midianko@gmail.com>
2025-05-15 21:09:27 +0100

    remove clg from proxy

--- api/proxy.ts
+++ api/proxy.ts
@@ -14,9 +14,6 @@ export default async function handler(req, res) {
   const token =
     'Yryr7KWg92Ms1B4Goqeuf-5R-dKBK4F3Obbb-fYSGTRTU6gLdLzG9rFjhE1BZNs7nHufxtjFcwXCUhtMbFgca0WReloJ1DZHj_hZigYtcF7x-SuZwUuvHIvqQMDgrG6JQ6NxCILAChdR_G6fy1Z9RzL_JcQoBDchXM9Cs5bAADq2Fjl--O4K0_4w4g-gPAPbwmHRHWDpUWwR7Ah0UdpCFiRkF3ssrhXI7k_C60kVU3IHXLWZ5EXAuS3MzHyE0ULV8tebADxH8qv6ni-rjAsiu2xt9T8w2lHtq2b_Px-U9_eEw4DoQGhMkO5IQAiMlVYnumwawDphfEp5yL5WgC8G88OHCM68AHEatCIZzcxTs6ffRGPa_yAXLMgs2CJYdERX2yX1Ut2CB7Q72yz8kHoMfoilZ90ECFPacUY4-j66I1nHnGBf4y39Yyg2swAG.Yryr7KWg92Ms1B4Goqeufw==';
 
-  console.log('Received query:', req.query);
-  console.log('Token:', token);
-
   if (!token) {
     return res.status(400).json({ error: 'Missing or invalid token' });
   }
@@ -28,14 +25,10 @@ export default async function handler(req, res) {
       headers: { jfwt: token },
     });
 
-    const text = await response.text();
-    console.log('External API response:', text);
-
     const data = await response.json();
     res.status(response.status).json(data);
     // eslint-disable-next-line
   } catch (err: any) {
-    console.error('Proxy error:', err);
     res
       .status(500)
       .json({ error: 'Internal Server Error', message: err.message });
